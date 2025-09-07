3432be1d8580f379e5c75c9068fb534762d22214
Mukoya Kuya <deltonmukoyakuya@gmail.com>
2025-09-08 00:22:44 +0300

    Changed directory

--- django-models/LibraryProject/relationship_app/models.py
+++ django-models/LibraryProject/relationship_app/models.py
@@ -1,5 +1,4 @@
 from django.db import models
-
 class Author(models.Model):
     name = models.CharField(max_length=100)
 
@@ -8,8 +7,8 @@ class Author(models.Model):
 
 
 class Book(models.Model):
-    title = models.CharField(max_length=100)
-    author = models.ForeignKey(Author, on_delete=models.CASCADE, related_name="books")
+    title = models.CharField(max_length=200)
+    author = models.ForeignKey(Author, on_delete=models.CASCADE)
 
     def __str__(self):
         return self.title
@@ -17,7 +16,7 @@ class Book(models.Model):
 
 class Library(models.Model):
     name = models.CharField(max_length=100)
-    books = models.ManyToManyField(Book, related_name="libraries")
+    books = models.ManyToManyField(Book)
 
     def __str__(self):
         return self.name
@@ -25,7 +24,7 @@ class Library(models.Model):
 
 class Librarian(models.Model):
     name = models.CharField(max_length=100)
-    library = models.OneToOneField(Library, on_delete=models.CASCADE, related_name="librarian")
+    library = models.OneToOneField(Library, on_delete=models.CASCADE)
 
     def __str__(self):
-        return self.name
+        return f"{self.name} - {self.library.name}"
