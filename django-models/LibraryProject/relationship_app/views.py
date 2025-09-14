2180133933db69571be92fa9ac68f6fdf1180676
Mukoya Kuya <122779322+MukoyaKuya@users.noreply.github.com>
2025-09-15 00:08:22 +0300

    Update views.py

--- django-models/LibraryProject/relationship_app/views.py
+++ django-models/LibraryProject/relationship_app/views.py
@@ -7,8 +7,7 @@ from .models import Book, Library
 # Function-based view to list all books
 def list_books(request):
     books = Book.objects.all()
-    return render(request, "list_books.html", {"books": books})
-
+    return render(request, "relationship_app/list_books.html", {"books": books})
 
 # Class-based view to show details for a specific library
 class LibraryDetailView(DetailView):
@@ -19,3 +18,4 @@ class LibraryDetailView(DetailView):
     def get_object(self):
         library_id = self.kwargs.get("pk")
         return get_object_or_404(Library, pk=library_id)
+
