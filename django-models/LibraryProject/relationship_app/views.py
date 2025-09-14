682c5333a762c000a24be865a341b31706cfbbd3
Mukoya Kuya <122779322+MukoyaKuya@users.noreply.github.com>
2025-09-15 00:12:19 +0300

    Update views.py

--- django-models/LibraryProject/relationship_app/views.py
+++ django-models/LibraryProject/relationship_app/views.py
@@ -12,7 +12,7 @@ def list_books(request):
 # Class-based view to show details for a specific library
 class LibraryDetailView(DetailView):
     model = Library
-    template_name = "library_detail.html"
+    template_name = "relationship_app/library_detail.html"  # <- update here
     context_object_name = "library"
 
     def get_object(self):
