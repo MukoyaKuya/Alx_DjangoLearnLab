a98ac0edacc53ff655e28fcd83ec346f1077421d
Mukoya Kuya <122779322+MukoyaKuya@users.noreply.github.com>
2025-09-15 00:43:40 +0300

    Update urls.py

--- django-models/LibraryProject/relationship_app/urls.py
+++ django-models/LibraryProject/relationship_app/urls.py
@@ -1,7 +1,10 @@
 from django.urls import path
-from .views import list_books, LibraryDetailView   # <- explicit imports
+from .views import list_books, LibraryDetailView, CustomLoginView, logout_view, register
 
 urlpatterns = [
-    path("books/", list_books, name="list_books"),  # FBV
-    path("library/<int:pk>/", LibraryDetailView.as_view(), name="library_detail"),  # CBV
+    path("books/", list_books, name="list_books"),
+    path("library/<int:pk>/", LibraryDetailView.as_view(), name="library_detail"),
+    path("login/", CustomLoginView.as_view(), name="login"),
+    path("logout/", logout_view, name="logout"),
+    path("register/", register, name="register"),
 ]
