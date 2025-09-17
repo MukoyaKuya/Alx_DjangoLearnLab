a793c67282c1f8f9fbd3022b8d17abc0fb64b358
Mukoya Kuya <122779322+MukoyaKuya@users.noreply.github.com>
2025-09-17 17:12:43 +0300

    Update views.py

--- django-models/LibraryProject/relationship_app/views.py
+++ django-models/LibraryProject/relationship_app/views.py
@@ -2,7 +2,27 @@ from django.contrib.auth.decorators import permission_required
 from django.contrib.auth.decorators import user_passes_test, login_required
 from django.shortcuts import render
 from .models import UserProfile
+from django.shortcuts import render, get_object_or_404, redirect
+from django.contrib.auth.decorators import permission_required
+from .models import Book
 
+@permission_required('relationship_app.can_add_book', raise_exception=True)
+def add_book(request):
+    # your logic to add a book
+    return render(request, 'relationship_app/add_book.html')
+
+@permission_required('relationship_app.can_change_book', raise_exception=True)
+def edit_book(request, book_id):
+    book = get_object_or_404(Book, id=book_id)
+    # your logic to edit the book
+    return render(request, 'relationship_app/edit_book.html', {'book': book})
+
+@permission_required('relationship_app.can_delete_book', raise_exception=True)
+def delete_book(request, book_id):
+    book = get_object_or_404(Book, id=book_id)
+    # your logic to delete the book
+    book.delete()
+    return redirect('book_list')
 
 # Helper functions to check role
 def is_admin(user):
@@ -38,4 +58,5 @@ def librarian_view(request):
 @user_passes_test(is_member, login_url="login")
 def member_view(request):
     return render(request, "relationship_app/member_view.html", {"user": request.user})
+
 
